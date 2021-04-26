c79cf8f87f4f3070283504f424a2ecd439beade6
Jon Anton <anton.jo@husky.neu.edu>
2021-04-25 21:38:39 -0400

    refactored, astar still not quite right

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,60 +1,36 @@
 import pygame
 import time
 import random
+import utils
+import copy
 
-
-class SnakeGame:
-
-    def __init__(self, displayWidth, displayHeight, displayCaption, blockSize, snakeSpeed, agent = None):
-        self.displayWidth = displayWidth
-        self.displayHeight = displayHeight
-        self.displayCaption = displayCaption
-        self.blockSize = blockSize
-        self.snakeSpeed = snakeSpeed
-        self.agent = agent
-
-        pygame.init()
-        self.display = pygame.display.set_mode((displayWidth, displayHeight))
-        self.clock = pygame.time.Clock()
-        self.fontStyle = pygame.font.SysFont("bahnschrift", 20)
-        self.scoreFont = pygame.font.SysFont("comicsansms", 20)
-        pygame.display.set_caption(displayCaption)
-
-        self.white = (255, 255, 255)
-        self.yellow = (255, 255, 102)
-        self.black = (0, 0, 0)
-        self.red = (213, 50, 80)
-        self.green = (0, 255, 0)
-        self.blue = (50, 153, 213)
-
-        self.initializeSnakeVariables()
-
-    def initializeSnakeVariables(self):
-        self.snakeLength = 1
-        self.snakeList = []
-        self.playerScore = 0
+class Snake:
+    def __init__(self, x, y, bounds, blockSize):
+        self.x1 = x
+        self.y1 = y
         self.x1Change = 0
         self.y1Change = 0
-        self.gameOver = False
-        self.gameClose = False
+        self.snakelist = []
+        self.snakelength = 1
+        self.bounds = bounds
+        self.blockSize = blockSize
         self.direction = None
-        self.x1 = self.displayWidth / 2
-        self.y1 = self.displayHeight / 2
-        self.foodx = round(random.randrange(0, self.displayWidth - self.blockSize) / 10.0) * 10.0
-        self.foody = round(random.randrange(0, self.displayHeight - self.blockSize) / 10.0) * 10.0
 
-    def updatePlayerScore(self):
-        self.playerScore = self.snakeLength - 1
-        value = self.scoreFont.render("Your Score: " + str(self.playerScore), True, self.yellow)
-        self.display.blit(value, [0, 0])
 
-    def drawSnake(self):
-        for x in self.snakeList:
-            pygame.draw.rect(self.display, self.black, [x[0], x[1], self.blockSize, self.blockSize])
+    # End game if snake collided with wall or itself
+    def checkCollisions(self, snakeHead):
+        if snakeHead in self.snakelist[:-1]:
+            return True
 
-    def message(self, msg, color):
-        mesg = self.fontStyle.render(msg, True, color)
-        self.display.blit(mesg, [self.displayWidth / 6, self.displayHeight / 3])
+        # Check if snake hit a wall
+        if snakeHead[0] >= self.bounds[0] \
+                or snakeHead[0] <= 0 \
+                or snakeHead[1] >= self.bounds[1] \
+                or snakeHead[1] <= 0:
+            return True
+
+        return False
+
 
     def move(self, keyEvent):
         if keyEvent == pygame.K_LEFT and self.direction != "RIGHT":
@@ -75,47 +51,7 @@ class SnakeGame:
             self.direction = "DOWN"
 
 
-    def gameOverScreen(self):
-        self.display.fill(self.blue)
-        self.message("You Lost! Press C-Play Again or Q-Quit", self.red)
-        # self.playerScore(self.snakeLength - 1)
-        pygame.display.update()
-
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    self.gameOver = True
-                    self.gameClose = False
-                if event.key == pygame.K_c:
-                    self.initializeSnakeVariables()
-                    self.gameLoop()
-
-    def eatFood(self):
-        if self.x1 == self.foodx and self.y1 == self.foody:
-            self.foodx = round(random.randrange(0, self.displayWidth - self.blockSize) / 10.0) * 10.0
-            self.foody = round(random.randrange(0, self.displayHeight - self.blockSize) / 10.0) * 10.0
-            self.snakeLength += 1
-
-
-    # End game if snake collided with wall or itself
-    def checkCollisions(self, snakeHead):
-        # End game if snake collides with itself
-        for x in self.snakeList[:-1]:
-            if x == snakeHead:
-                return True
-
-        # Check if snake hit a wall
-        if self.x1 >= (self.displayWidth - self.blockSize) \
-                or self.x1 <= 0 \
-                or self.y1 >= (self.displayHeight-self.blockSize) \
-                or self.y1 <= 0:
-            return True
-
-        return False
-
-    def getChange(self, keyEvent):
-        xChange = 0
-        yChange = 0
+    def getChange(self, keyEvent,xChange,yChange):
         direction = None
 
         if keyEvent == pygame.K_LEFT and self.direction != "RIGHT":
@@ -141,7 +77,7 @@ class SnakeGame:
     def getValidActions(self):
         actions = []
         for action in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
-            (xChange, yChange, direction) = self.getChange(action)
+            (xChange, yChange, direction) = self.getChange(action,self.x1Change,self.y1Change)
             if not direction:
                 continue
 
@@ -154,6 +90,99 @@ class SnakeGame:
         return (snakehead[0] + change[0], snakehead[1] + change[1])
 
 
+    def getSuccessors(self):
+        successors = []
+        for action, position in self.getValidActions():
+            newSnake = copy.deepcopy(self)
+            newSnake.x1 = position[0]
+            newSnake.y1 = position[1]
+            newSnake.move2(position)
+            successors.append((newSnake, action))
+
+        return successors
+
+    def move2(self,snakeHead):
+        self.snakelist.append(snakeHead)
+        if len(self.snakelist) > self.snakelength:
+            del self.snakelist[0]
+
+
+class SnakeGame:
+
+    def __init__(self, displayWidth, displayHeight, displayCaption, blockSize, snakeSpeed, agent = None):
+        self.displayWidth = displayWidth
+        self.displayHeight = displayHeight
+        self.blockSize = blockSize
+        self.bounds = (self.displayWidth - self.blockSize, self.displayHeight - self.blockSize)
+        self.snakeSpeed = snakeSpeed
+        self.agent = agent
+
+        pygame.init()
+        self.display = pygame.display.set_mode((displayWidth, displayHeight))
+        self.clock = pygame.time.Clock()
+        self.fontStyle = pygame.font.SysFont("bahnschrift", 20)
+        self.scoreFont = pygame.font.SysFont("comicsansms", 20)
+        pygame.display.set_caption(displayCaption)
+
+        self.white = (255, 255, 255)
+        self.yellow = (255, 255, 102)
+        self.black = (0, 0, 0)
+        self.red = (213, 50, 80)
+        self.green = (0, 255, 0)
+        self.blue = (50, 153, 213)
+
+        self.initializeSnakeVariables()
+
+    def initializeSnakeVariables(self):
+        self.playerScore = 0
+        self.gameOver = False
+        self.gameClose = False
+        self.snake = Snake(self.displayWidth / 2, self.displayHeight / 2, self.bounds, self.blockSize)
+        self.spawnFood()
+
+    def spawnFood(self):
+        self.foodx = round(random.randrange(self.blockSize, self.displayWidth - (self.blockSize * 2)) / self.blockSize) * self.blockSize
+        self.foody = round(random.randrange(self.blockSize, self.displayHeight - (self.blockSize * 2)) / self.blockSize) * self.blockSize
+
+    def updatePlayerScore(self):
+        self.playerScore = self.snake.snakelength - 1
+        value = self.scoreFont.render("Your Score: " + str(self.playerScore), True, self.yellow)
+        self.display.blit(value, [0, 0])
+
+    def drawSnake(self):
+        for x in self.snake.snakelist:
+            pygame.draw.rect(self.display, self.black, [x[0], x[1], self.blockSize, self.blockSize])
+
+    def message(self, msg, color):
+        mesg = self.fontStyle.render(msg, True, color)
+        self.display.blit(mesg, [self.displayWidth / 6, self.displayHeight / 3])
+
+
+
+
+    def gameOverScreen(self):
+        self.display.fill(self.blue)
+        self.message("You Lost! Press C-Play Again or Q-Quit", self.red)
+        # self.playerScore(self.snake.snakelength - 1)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    self.gameOver = True
+                    self.gameClose = False
+                if event.key == pygame.K_c:
+                    self.initializeSnakeVariables()
+                    self.gameLoop()
+
+    def eatFood(self):
+        if self.snake.x1 == self.foodx and self.snake.y1 == self.foody:
+            self.spawnFood()
+            self.snake.snakelength += 1
+
+
+
+
 
     def gameLoop(self):
         while not self.gameOver:
@@ -162,35 +191,29 @@ class SnakeGame:
             while self.gameClose:
                 self.gameOverScreen()
 
-
-
             if self.agent:
-                nextMove = self.agent.selectMove((self.x1, self.y1), (self.foodx, self.foody), self.getValidActions())
-
-                newEvent = pygame.event.Event(pygame.KEYDOWN, unicode="a", key=nextMove,
-                                              mod=pygame.KMOD_NONE)
-                pygame.event.post(newEvent)
-
-
+                nextMove = self.agent.selectMove(self.snake, (self.foodx, self.foody))
+                if nextMove:
+                    newEvent = pygame.event.Event(pygame.KEYDOWN, unicode="a", key=nextMove,
+                                                  mod=pygame.KMOD_NONE)
+                    pygame.event.post(newEvent)
 
             # Move the snake, or close the game window
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.gameOver = True
                 if event.type == pygame.KEYDOWN:
-                    self.move(event.key)
+                    self.snake.move(event.key)
 
             # Move snake
-            self.x1 += self.x1Change
-            self.y1 += self.y1Change
+            self.snake.x1 += self.snake.x1Change
+            self.snake.y1 += self.snake.y1Change
             self.display.fill(self.blue)
             pygame.draw.rect(self.display, self.green, [self.foodx, self.foody, self.blockSize, self.blockSize])
-            snakeHead = [self.x1, self.y1]
-            self.snakeList.append(snakeHead)
-            if len(self.snakeList) > self.snakeLength:
-                del self.snakeList[0]
+            snakeHead = [self.snake.x1, self.snake.y1]
+            self.snake.move2(snakeHead)
 
-            if self.checkCollisions(snakeHead):
+            if self.snake.checkCollisions(snakeHead):
                 self.gameClose = True
 
             self.drawSnake()
@@ -205,19 +228,59 @@ class SnakeGame:
         quit()
 
 
-class AStarAgent:
 
-    def selectMove(self, snake, food, validActions):
+
+class ManhattanAgent:
+
+    def selectMove(self, snake, food):
         bestDistance = float("inf")
         bestAction = None
 
-        for action, position in validActions:
+        for action, position in snake.getValidActions():
             distance = manhattanHeuristic(position, food)
             if distance < bestDistance:
                 bestDistance = distance
                 bestAction = action
 
         return bestAction
+
+
+class AStarAgent:
+
+    def __init__(self):
+        self.path = []
+
+    def selectMove(self, snake, food):
+        if not self.path:
+            self.path = self.computePath(snake,food)
+            print(self.path)
+
+            if not self.path:
+                return None
+
+        nextMove = self.path.pop()
+        return nextMove
+
+    def computePath(self, snake, food):
+        frontier = utils.myPriorityQueue(food, self.aStarHelperFn)
+        state = (snake,[])
+        explored = set()
+        frontier.push(state, manhattanHeuristic)
+
+        while not frontier.isEmpty():
+            node = frontier.pop()
+            if (node[0].x1, node[0].y1) == food:
+                return node[1]
+            if node[0] in explored:
+                continue
+            explored.add(node[0])
+            for child in node[0].getSuccessors():
+                if child[0] not in explored:
+                    frontier.push((child[0], node[1]+[child[1]]), manhattanHeuristic)
+
+    #state = (snake,list of actions so far)
+    def aStarHelperFn(self, state, goal, heuristic):
+        return len(state[1]) + heuristic((state[0].x1, state[0].y1), goal)
 
 
 # Referenced from pacman project
@@ -227,14 +290,18 @@ def manhattanHeuristic(xy1, xy2):
 
 
 
-# Food collision only works with blockSize = 10 right now
+
+
 width = 400
 height = 400
 message = "Play Snake"
-blockSize = 10
-speed = 100
+blockSize = 20
+speed = 10
 
-agent = AStarAgent()
+agent = ManhattanAgent()
+aStarAgent = AStarAgent()
 
 game = SnakeGame(width, height, message, blockSize, speed, agent)
+# game = SnakeGame(width, height, message, blockSize, speed, aStarAgent)
+
 game.gameLoop()
